d32402f636068299a557e6fff315e7ca543cec7e
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-07-09 12:12:11 +0330

    Update settings.py

--- OnlineSchool/OnlineSchool/settings.py
+++ OnlineSchool/OnlineSchool/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^sk2l6wwn17bkzuyf4a#=6o88#x+&1oq42uk3f05lpu77ks2^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
